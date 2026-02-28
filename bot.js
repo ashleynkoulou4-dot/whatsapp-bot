2c12643bbcabf7d062b29a72dec1ac25b12472b3
ashleynkoulou4-dot <ashleynkoulou4@gmail.com>
2026-02-28 20:17:16 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,8 +7,14 @@ async function startBot() {
     const { state, saveCreds } = await useMultiFileAuthState('auth_info');
     const sock = makeWASocket({
         auth: state,
-        printQRInTerminal: true, // affiche le QR code dans le terminal
-        logger: P({ level: 'silent' })
+       logger: P({ level: 'silent' })
+          });
+    // écouter les mises à jour de connexion
+    sock.ev.on('connection.update', (update) => {
+        const { qr } = update;
+        if (qr) {
+            console.log('QR code reçu, scanne-le avec WhatsApp:', qr);
+            }
         });
     sock.ev.on('creds.update', saveCreds);
     sock.ev.on('messages.upsert', async (msg) => {
