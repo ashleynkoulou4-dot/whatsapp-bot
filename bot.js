862d2f2cf3e1f3abd4c758858d2e3d64f9805cd5
ashleynkoulou4-dot <ashleynkoulou4@gmail.com>
2026-02-28 20:36:56 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,16 +9,20 @@ async function startBot() {
         auth: state,
        logger: P({ level: 'silent' })
           });
-    // écouter les mises à jour de connexion
-    sock.ev.on('connection.update', (update) => {
-        const { qr } = update;
-        if (qr) {
-            console.log('QR code reçu, scanne-le avec WhatsApp:', qr);
-            }
-        });
     sock.ev.on('creds.update', saveCreds);
+    // Afficher le QR code
+        sock.ev.on('connection.update', (update) => {
+        const { qr, connection } = update;
+            if (qr) {
+            console.log('QR code reçu, scanne-le avec WhatsApp:', qr);
+        }
+    if (connection === 'open') {
+    console.log('✅ Bot connecté à WhatsApp');
+        }
+        });
+    // Répondre aux messages
     sock.ev.on('messages.upsert', async (msg) => {
-        const m = msg.messages[0];
+       const m = msg.messages[0];
         if (!m.message) return;
         const text = m.message.conversation;
         console.log('Message reçu:', text);
